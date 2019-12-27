fe8e321f59d1b7b1752f6b3c7cd9f9eb93a3dc54
Lazar Marković <lazar.kmarkovic@gmail.com>
2019-12-27 16:43:52 +0100

    Update changer.c

--- changer.c
+++ changer.c
@@ -2,9 +2,9 @@
 /*  Changer is linux-based (anti-)forensics tool for reading,    */
 /*  extracting, modifying and securely deleting precisely        */
 /*  selected either indexed or deleted data from files or whole  */
-/*  disks using. Basically, it's srm (secure_delete) but on      */
-/*  manually defined scale with surgical precision and bunch of  */
-/*  other options.                                               */
+/*  disks. Basically, it's srm (secure_delete) but on manually   */
+/*  defined scale with surgical precision and bunch of  other    */
+/*  options.                                                     */
 /*  Coded by Lazar Marković for no special reason.               */                         
 
 /*  This code is free; you can redistribute it and/or            */
